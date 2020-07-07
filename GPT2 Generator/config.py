ee290933350611d1a6d96d368d4594fd4f7e4bbc
Arpan Mishra <mishraarpan6@gmail.com>
2020-07-07 14:49:40 +0530

    Update config.py

--- GPT2 Generator/config.py
+++ GPT2 Generator/config.py
@@ -4,6 +4,6 @@ batch_size = 10
 model_path = 'gpt2_epoch5.bin'
 max_seq_len = 300
 epochs = 5
-data_path = 'C:/Users/Arpan/Downloads/Education/Project/Project-Anime/eda-data.csv'
+data_path = 'Data/eda-data.csv'
 tokenizer = transformers.GPT2Tokenizer.from_pretrained('gpt2')
 model = transformers.GPT2LMHeadModel.from_pretrained('gpt2')
